c2a9a4386312eff26d90c64738212e75c6d256bc
Lamine Diallo <bah07565@gmail.com>
2025-07-31 13:20:27 +0000

    feat(dao): implement IClasseDao and ISectorDao interfaces

--- src/main/java/com/lamine/api/dao/IClasseDao.java
+++ src/main/java/com/lamine/api/dao/IClasseDao.java
@@ -1,4 +1,11 @@
 package com.lamine.api.dao;
 
-public interface IClasseDao {
+import com.lamine.api.models.Classe;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+
+@Repository
+public interface IClasseDao extends JpaRepository<Classe, Long> {
+    boolean existsByClassNameIgnoreCaseAndSectorId(String className, Long sectorId);
 }
--- src/main/java/com/lamine/api/dao/ISectorDao.java
+++ src/main/java/com/lamine/api/dao/ISectorDao.java
@@ -1,4 +1,11 @@
 package com.lamine.api.dao;
 
-public interface ISectorDao {
+import com.lamine.api.models.Sector;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+
+@Repository
+public interface ISectorDao extends JpaRepository<Sector, Long> {
+    boolean existsByNameIgnoreCase(String name);
 }
